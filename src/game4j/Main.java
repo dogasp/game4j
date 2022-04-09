03dcda58332562658652ce6ddcad2aa68aa58a8c
dogasp <dorian.gaspar@orange.fr>
2022-04-09 13:04:33 +0200

    testing

--- src/game4j/Main.java
+++ src/game4j/Main.java
@@ -4,15 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("game4j.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("game4j.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root, 400, 300));*/
+
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
